bda2b6c4f3b837e5b5f94d6262c4f017dd20c9e6
dev-vaayen <122223761+dev-vaayen@users.noreply.github.com>
2024-11-22 12:26:27 +0530

    Update main.go

--- main.go
+++ main.go
@@ -10,55 +10,143 @@ import (
 	"github.com/rivo/tview"
 )
 
+var app *tview.Application
+var globalEmail, globalPassword string
+
 func main() {
-	app := tview.NewApplication()
 	// main page FRONTEND
+	app = tview.NewApplication()
+	loginPage()
+}
+
+func loginPage() {
+	globalEmail = ""
+	globalPassword = ""
 	form := tview.NewForm().SetButtonsAlign(tview.AlignCenter).
-		AddInputField("From:", "", 25, nil, nil).
-		// AddPasswordField("From:", "", 25, '*', nil).
-		AddPasswordField("Password:", "", 25, '*', nil). // passwd is google application password not the actual
-		// https://support.google.com/accounts/answer/185833?hl=en <<<<------ pls see
+//		AddInputField("From:", "", 25, nil, nil).
+		AddPasswordField("From:", "", 25, '*', nil).
+		AddPasswordField("Password:", "", 25, '*', nil) // passwd is google application password not the actual
+	// https://support.google.com/accounts/answer/185833?hl=en <<<<------ pls see
+
+	form.AddButton("LOGIN!", func() {
+		emailItem := form.GetFormItem(0).(*tview.InputField)
+		passwordItem := form.GetFormItem(1).(*tview.InputField)
+
+		// how to put gmail picture side by side?
+		email := emailItem.GetText()
+		password := passwordItem.GetText()
+
+		err := loginFromThisDeviceAlert(email, password) // needs to be fixed // FIX WHAT?!?!!?!?
+		if err != nil {
+			showDialog(app, fmt.Sprintf("Login failed: %v", err), form)
+		} else {
+			// once user is logged into Gmail, they will be sent to next window to compose a mail
+			globalEmail = email
+			globalPassword = password
+			landingPageMaybeLikeWebUI() // go to composeMail directly on successful login
+		}
+	}).AddButton("Exit", func() {
+		app.Stop()
+	})
+	// form.AddImage // learnhow this works - can set up logo on login page
+	form.SetBorder(true).SetTitle(" Gmail-TUI ").SetTitleAlign(tview.AlignCenter)
+	form.SetButtonsAlign(tview.AlignCenter)
+	app.SetRoot(form, true).EnableMouse(true).EnablePaste(true) // Set login form as the initial root
+	if err := app.Run(); err != nil {
+		log.Fatalf("Error starting application: %v", err)
+	}
+}
+
+func landingPageMaybeLikeWebUI() {
+	form := tview.NewForm().SetButtonsAlign(tview.AlignCenter)
+
+	form.AddButton("Compose Mail", func() {
+		composeMail()
+	}).AddButton("Inbox (WIP)", func() {
+
+	}).AddButton("Drafts (WIP)", func() {
+
+	}).AddButton("Starred (WIP)", func() {
+
+	}).AddButton("Exit", func() {
+		app.Stop()
+		app.Stop()
+	})
+	form.SetBorder(true).SetTitle(" Gmail-TUI ").SetTitleAlign(tview.AlignCenter)
+	form.SetButtonsAlign(tview.AlignCenter).SetTitleAlign(tview.AlignLeft)
+	app.SetRoot(form, true).EnableMouse(true).EnablePaste(true) // Set login form as the initial root
+	if err := app.Run(); err != nil {
+		log.Fatalf("Error starting application: %v", err)
+	}
+}
+
+// In login page, login is being validated based on whether or not mail could be sent to self using entered email and password
+func loginFromThisDeviceAlert(email, password string) error {
+	smtpServer := "smtp.gmail.com"
+	smtpPort := "587"
+	auth := smtp.PlainAuth("", email, password, smtpServer)
+
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	ip, err := getLocalIP()
+	if err != nil {
+		return fmt.Errorf("failed to get local IP address: %w", err)
+	}
+
+	to := []string{email}
+	msg := []byte("To: " + email + "\r\n" +
+		"Subject: " + email + "\r\n" +
+		fmt.Sprintf("\r\nGmail-TUI Login Alert on a device with %s IP address at %s.\r\n", ip, timestamp)) /// <--- this is success
+
+	// actual execution of sending
+	err = smtp.SendMail(smtpServer+":"+smtpPort, auth, email, to, msg)
+	if err != nil {
+		return fmt.Errorf("failed to send email: %w", err)
+	}
+
+	return nil
+}
+
+func composeMail() {
+	form := tview.NewForm().SetButtonsAlign(tview.AlignCenter).
 		AddInputField("To:", "", 25, nil, nil).
 		AddInputField("Subject:", "", 66, nil, nil).
 		AddTextArea("Mail-Body:", "", 66, 0, 0, nil)
 
 		// button configurations and what goes behind them kinda like their backend
 	form.AddButton("SEND!", func() {
-		emailItem := form.GetFormItem(0).(*tview.InputField)
-		passwordItem := form.GetFormItem(1).(*tview.InputField)
-		toEMAIL := form.GetFormItem(2).(*tview.InputField)
-		subjectMAIL := form.GetFormItem(3).(*tview.InputField)
-		bodyMAIL := form.GetFormItem(4).(*tview.TextArea)
+		toEMAIL := form.GetFormItem(0).(*tview.InputField)
+		subjectMAIL := form.GetFormItem(1).(*tview.InputField)
+		bodyMAIL := form.GetFormItem(2).(*tview.TextArea)
 
 		// how to put gmail picture side by side?
-		email := emailItem.GetText()
-		password := passwordItem.GetText()
 		toEmailId := toEMAIL.GetText()
 		subject := subjectMAIL.GetText()
 		mailbody := bodyMAIL.GetText()
 
-		err := sendLoginNotification(email, password, toEmailId, subject, mailbody) // needs to be fixed // FIX WHAT?!?!!?!?
+		err := sendSmtpMailLogic(globalEmail, globalPassword, toEmailId, subject, mailbody) // needs to be fixed // FIX WHAT?!?!!?!?
 		if err != nil {
 			showDialog(app, fmt.Sprintf("failed to send email: %v", err), form)
 		} else {
 			showDialog(app, "Gmail sent successfully - Please check!", form)
 		}
 	}).
-		AddButton("QUIT", func() {
-			app.Stop()
+		AddButton("Back", func() {
+			landingPageMaybeLikeWebUI()
 		})
-	form.SetBorder(true).SetTitle(" Gmail-TUI by Dev_Vaayen ").SetTitleAlign(tview.AlignCenter)
+	form.SetBorder(true).SetTitle(" Gmail-TUI ").SetTitleAlign(tview.AlignCenter)
 
-	if err := app.SetRoot(form, true).Run(); err != nil {
+	if err := app.SetRoot(form, true).EnableMouse(true).EnablePaste(true).Run(); err != nil {
 		log.Fatalf("Error starting application: %v", err)
 	}
+
 }
 
-// accepts stuff from main func and uses it to send mail
-func sendLoginNotification(email, password, toEmailId, subject, mailbody string) error {
+// for accepting stuff from main func and uses it to send mail - after test will remove timestamping in mails
+func sendSmtpMailLogic(globalEmail, globalPassword, toEmailId, subject, mailbody string) error {
+	// https://www.geeksforgeeks.org/sending-email-using-smtp-in-golang/ <---- implementation of understanding from here
 	smtpServer := "smtp.gmail.com"
 	smtpPort := "587"
-	auth := smtp.PlainAuth("", email, password, smtpServer)
+	auth := smtp.PlainAuth("", globalEmail, globalPassword, smtpServer)
 
 	timestamp := time.Now().Format("2006-01-02 15:04:05")
 	ip, err := getLocalIP()
@@ -73,7 +161,7 @@ func sendLoginNotification(email, password, toEmailId, subject, mailbody string)
 		fmt.Sprintf("===================================================================\r\nThis mail was sent on %s from IP address %s.\r\n", timestamp, ip))
 
 	// actual execution of sending
-	err = smtp.SendMail(smtpServer+":"+smtpPort, auth, email, to, msg)
+	err = smtp.SendMail(smtpServer+":"+smtpPort, auth, globalEmail, to, msg)
 	if err != nil {
 		return fmt.Errorf("failed to send email: %w", err)
 	}
@@ -81,7 +169,7 @@ func sendLoginNotification(email, password, toEmailId, subject, mailbody string)
 	return nil
 }
 
-// for security purpose also sending where the mail was sent from
+// for security purpose also sending where the mail was sent from --->
 // https://stackoverflow.com/a/37382208  <---- major influence
 func getLocalIP() (string, error) {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
@@ -104,8 +192,9 @@ func getLocalIP() (string, error) {
 // 	app.SetRoot(inboxView, true).SetFocus(inboxView)
 // }
 
-// after sending the mail user should learn whether mail was sent or not
+// after sending the mail user should learn whether or not mail was sent - shown uses dialog box
 func showDialog(app *tview.Application, message string, returnTo tview.Primitive) {
+	// ----------> https://github.com/rivo/tview/wiki/Modal  <----------- very important 
 	modal := tview.NewModal().
 		SetText(message).
 		AddButtons([]string{"Return To Main Page"}).
@@ -114,6 +203,5 @@ func showDialog(app *tview.Application, message string, returnTo tview.Primitive
 				app.SetRoot(returnTo, true).SetFocus(returnTo)
 			}
 		})
-
 	app.SetRoot(modal, true).SetFocus(modal)
 }
